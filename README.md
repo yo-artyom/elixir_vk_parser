eea00408d6dae24f6b3abbb4cb13e6a8ca7acd54
Artem <q3pp@yandex.ru>
2018-06-18 15:11:19 +0200

    update readme

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ A quite fast scrapper of different stuff from VK
 `mix deps.get`
 
 ## Config
-You need to set a VK group in `config/config.exs`
+You need to setup the group names in `group_params.json` (See an example inside).
 
 Also, need to set up access_token below in `config/config.exs`
 
 ### Start application
-`mix run --no-halt`
-
+`mix run -e  'VkParser.start_wall_flow' --no-halt`
 
 ### Capabilities:
 * Right now only image scraping is possible
--- lib/vk_parser/wall/analytics/math.ex
+++ lib/vk_parser/wall/analytics/math.ex
@@ -2,18 +2,21 @@ defmodule VkParser.Wall.Analytics.Math do
 
   def median(group, column \\ :likes_count) do
     VkParser.Wall.PostsStorage.state 
+    |> Stream.filter( fn(post) -> post.group_name == group end)
     |> Enum.map(&(Map.get(&1, column)))
     |> Statistics.median
   end
 
   def mean(group, column \\ :likes_count) do
     VkParser.Wall.PostsStorage.state 
+    |> Stream.filter( fn(post) -> post.group_name == group end)
     |> Enum.map(&(Map.get(&1, column)))
     |> Statistics.mean
   end
 
   def variance(group, column \\ :likes_count) do
     VkParser.Wall.PostsStorage.state 
+    |> Stream.filter( fn(post) -> post.group_name == group end)
     |> Enum.map(&(Map.get(&1, column)))
     |> Statistics.mean
   end
